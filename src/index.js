2f4414674099ee48054913b92ce6bcac3457196e
Maciej Mądry <maciejmadry0@gmail.com>
2022-11-28 20:20:12 +0100

    added: routing and base file structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HomePage from 'components/pages/HomePage/HomePage';
+import MainLayout from 'components/layouts/MainLayout/MainLayout';
+import AddMoviePage from 'components/pages/AddMoviePage/AddMoviePage';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'add',
+        element: <AddMoviePage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
